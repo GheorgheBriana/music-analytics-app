966dc3d479a13b6d427606564f9e53c971c747e9
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-04-15 15:57:18 +0300

    Initialize React app and add dashboard title

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <h1> All Time Wrapped Dashboard </h1>
+            <p> Your music story, all in one place. </p>
+        </div>
+    )
+}
+
+export default App
