ef3ec9b97a7cc9153f0b82472fc6d4411244b458
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-04-15 16:16:08 +0300

    Change title from landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className = "landing-page">
             <div className = "hero-section">
-                <h1> All Time Wrapped Dashboard </h1>
+                <h1> All Time Wrapped</h1>
                 <p>Discover your music story anytime, not just once a year.</p>
 
                 <div className = "hero-buttons">
