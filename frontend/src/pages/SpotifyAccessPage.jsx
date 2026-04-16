d3232a959471b67c8c9503cb2b547506492ea42c
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-04-16 11:34:56 +0300

    Move landing page into separate component

---
+++ frontend/src/pages/SpotifyAccessPage.jsx
@@ -0,0 +1,10 @@
+function SpotifyAccessPage() {
+    return (
+        <div>
+            <h1>Spotify Access</h1>
+            <p>Connect your Spotify account to explore your music insights automatically.</p>
+        </div>
+    )
+}
+
+export default SpotifyAccessPage
