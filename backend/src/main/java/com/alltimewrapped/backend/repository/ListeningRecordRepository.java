a96ed11a4a0cd84c31cd4c110870e750bde31f2b
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-04-27 16:08:26 +0300

    Add ListeningRecord repository

---
+++ backend/src/main/java/com/alltimewrapped/backend/repository/ListeningRecordRepository.java
@@ -0,0 +1,14 @@
+package com.alltimewrapped.backend.repository;
+
+import com.alltimewrapped.backend.model.AppUser;
+import com.alltimewrapped.backend.model.ListeningRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ListeningRecordRepository extends JpaRepository<ListeningRecord, Long> {
+
+    List<ListeningRecord> findByUser(AppUser user);
+
+    List<ListeningRecord> findByUserOrderByPlayedAtDesc(AppUser user);
+}
